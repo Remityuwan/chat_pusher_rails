d2513d7dcafa9d6eb1788e27caf0f40657cdb301
Remityuwan <ht11a021@oecu.jp>
2014-12-09 12:40:56 +0900

    web_chat_system Finish

---
+++ vendor/bundle/ruby/2.0.0/gems/json-1.8.1/ext/json/ext/generator/siteconf20141204-10676-qnnnah.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20141204-10676-1cv11wy"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
